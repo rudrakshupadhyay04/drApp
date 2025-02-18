43c5a9930e7afd022acedfb69f3ae329d0a78f58
rudrakshupadhyay04 <149106390+rudrakshupadhyay04@users.noreply.github.com>
2025-02-18 12:12:09 +0530

    basic server is created

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,14 @@
 import express from "express";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.PORT;
+app.listen(port, () => {
+    console.log(`server is running at port ${port}`);
+})
